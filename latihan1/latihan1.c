3df7f6a67b41b53b7bcb0d650e96c41c1ee55594
cakraandika <cakraandika28@gmail.com>
2018-10-08 13:40:18 +0700

    massage

---
+++ latihan1/latihan1.c
@@ -0,0 +1,12 @@
+#include<iostream.h>
+int main() {
+int A,T;
+float L;
+A = 5;
+T = 7;
+L = A*T/2.0;
+std::cout<<"Program Menghitung Luas Segitiga"<< std::endl;
+std::cout<<"Alas Segitiga = "<<A<< std::endl;
+std::cout<<"Tinggi Segitiga = "<<T<< std::endl
+std::cout<< "Luas Segitiga adalah = "<<L<< std::endk;
+}
